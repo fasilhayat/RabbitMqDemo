3da06bc7d9b2786a6788c443df58e94b451c3bf1
Fasil Malik Hayat <fmh@forca.dk>
2023-04-15 15:22:15 +0200

    Add project files.

---
+++ RabbitMqDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using Forca.Datamodel.Entiteter;
+using RabbitMqDemo;
+
+#region OldCode
+//var factory = new ConnectionFactory
+//{
+//    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+//    ClientProvidedName = "RabbitMQ -message sender"
+//};
+
+//var connection = factory.CreateConnection();
+//var channel = connection.CreateModel();
+
+//var exchangeName = "PensionsdataExchange";
+//var routingKey = "Pensionsdata-routing-key";
+//var queueName = "PensionsdataQueue";
+
+//channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+//channel.QueueDeclare(queueName, false, false, false, null);
+//channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+//var jsonData = File.ReadAllText(@"~\..\Data\0101010202_pbu.json");
+
+//var messageBody = Encoding.UTF8.GetBytes(jsonData);
+//channel.BasicPublish(exchangeName, routingKey, null, messageBody);
+
+//channel.Close();
+//connection.Close(); 
+#endregion
+
+const string conn = "amqp://guest:guest@localhost:5672";
+const string clientProviderName = "RabbitMQ -message sender";
+
+const string exchangeName = "PensionsdataExchange";
+const string routingKey = "Pensionsdata-routing-key";
+const string queue = "PensionsdataQueue";
+
+var queueConnect = new QueueConnection(conn, clientProviderName, exchangeName, routingKey, queue);
+
+// TESTDATA
+// Returner enten data fra køen eller indlæser json fil - TODO: skal rettes så der altid hentes fra REDIS
+var testdata = File.ReadAllText(@"~\..\..\..\..\..\RabbitMqDemo\Data\0101010202_pbu.json");
+var message = JsonSerializer.Deserialize<Pensionsdata>(testdata) ?? new Pensionsdata();
+
+queueConnect.PublishMessage(message);
---
+++ RabbitMqDemo/QueueConnection.cs
@@ -0,0 +1,60 @@
+﻿namespace RabbitMqDemo
+{
+    using RabbitMQ.Client;
+    using System.Text;
+    using System.Text.Json;
+
+    public class QueueConnection
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly string _exchangeName;
+        private readonly string _routingKey;
+        private readonly string _queueName;
+
+        public QueueConnection(string endpoint, string clientProviderName, string exchangeName, string routingKey, string queueName)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(endpoint),
+                ClientProvidedName = clientProviderName
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            _exchangeName = exchangeName;
+            _routingKey = routingKey;
+            _queueName = queueName;
+        }
+
+        public void PublishMessage<T>(T message) where T : class
+        {
+            OpenChannel();
+            
+            var data = JsonSerializer.Serialize(message);
+            var messageBody = Encoding.UTF8.GetBytes(data);
+            _channel.BasicPublish(_exchangeName, _routingKey, null, messageBody);
+
+            CloseChannel();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void OpenChannel()
+        {
+            _channel.ExchangeDeclare(_exchangeName, ExchangeType.Direct);
+            _channel.QueueDeclare(_queueName, false, false, false, null);
+            _channel.QueueBind(_queueName, _exchangeName, _routingKey, null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CloseChannel()
+        {
+            _channel.Close();
+            _connection.Close();
+        }
+    }
+}
