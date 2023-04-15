1fd04463a571d6e04a299babadea103d6fbb2d9e
Fasil Malik Hayat <fmh@forca.dk>
2023-04-15 21:14:16 +0200

    Udvidet med nservicebus (virker ikke endnu)

---
+++ RabbitMqDemo/MutateOutgoingMessages.cs
@@ -0,0 +1,29 @@
+﻿namespace RabbitMqDemo
+{
+    using NServiceBus.MessageMutator;
+
+    public class MutateOutgoingMessages : IMutateOutgoingTransportMessages
+    {
+        public Task MutateOutgoing(MutateOutgoingTransportMessageContext context)
+        {
+            // the outgoing headers
+            var outgoingHeaders = context.OutgoingHeaders;
+
+            if (context.TryGetIncomingMessage(out var incomingMessage))
+            {
+                // do something with the incoming message
+            }
+
+            if (context.TryGetIncomingHeaders(out var incomingHeaders))
+            {
+                // do something with the incoming headers
+            }
+
+            // the outgoing message
+            // optionally replace the message instance by setting context.OutgoingMessage
+            var outgoingMessage = context.OutgoingMessage;
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- RabbitMqDemo/Program.cs
+++ RabbitMqDemo/Program.cs
@@ -1,33 +1,7 @@
 ﻿using System.Text.Json;
 using Forca.Datamodel.Entiteter;
+using NServiceBus.MessageMutator;
 using RabbitMqDemo;
-
-#region OldCode
-//var factory = new ConnectionFactory
-//{
-//    Uri = new Uri("amqp://guest:guest@localhost:5672"),
-//    ClientProvidedName = "RabbitMQ -message sender"
-//};
-
-//var connection = factory.CreateConnection();
-//var channel = connection.CreateModel();
-
-//var exchangeName = "PensionsdataExchange";
-//var routingKey = "Pensionsdata-routing-key";
-//var queueName = "PensionsdataQueue";
-
-//channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
-//channel.QueueDeclare(queueName, false, false, false, null);
-//channel.QueueBind(queueName, exchangeName, routingKey, null);
-
-//var jsonData = File.ReadAllText(@"~\..\Data\0101010202_pbu.json");
-
-//var messageBody = Encoding.UTF8.GetBytes(jsonData);
-//channel.BasicPublish(exchangeName, routingKey, null, messageBody);
-
-//channel.Close();
-//connection.Close(); 
-#endregion
 
 const string conn = "amqp://guest:guest@localhost:5672";
 const string clientProviderName = "RabbitMQ -message sender";
@@ -35,6 +9,9 @@ const string clientProviderName = "RabbitMQ -message sender";
 const string exchangeName = "PensionsdataExchange";
 const string routingKey = "Pensionsdata-routing-key";
 const string queue = "PensionsdataQueue";
+
+var endpointConfiguration = new NServiceBus.EndpointConfiguration("myNservicebusEndpoint");
+endpointConfiguration.RegisterMessageMutator(new MutateOutgoingMessages());
 
 var queueConnect = new QueueConnection(conn, clientProviderName, exchangeName, routingKey, queue);
 
